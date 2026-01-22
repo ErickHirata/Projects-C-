2497237d8cbf3c7c2b701f5a982187cb7f22b4fb
erick yuji hirata <134017989+ErickHirata@users.noreply.github.com>
2026-01-21 23:37:10 -0300

    Adicao do Projeto de Variaveis

---
+++ Constantes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjetoVariaveis
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int Number = 30;
+            Number = 20;
+            Console.WriteLine("O Antigo Valor da variável era: " + Number);
+
+            Number = 10;
+            Console.WriteLine("O Novo Valor da variável é: " + Number);
+
+            string name = "Erick";
+            Console.WriteLine("O Nome era: " + name);
+
+            name = "João";
+            Console.WriteLine("O Novo Nome é: " + name);
+        }
+    }
+}
