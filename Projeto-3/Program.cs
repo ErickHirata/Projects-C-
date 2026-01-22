7067024ab9f8144bcc34b0a5972336cd163fb1e4
erick yuji hirata <134017989+ErickHirata@users.noreply.github.com>
2026-01-21 23:12:08 -0300

    adicao do projeto 3

---
+++ Projeto-3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Projeto2;
+public class Program
+{   
+    public static void Main(string[] args)
+    {
+        string MyName = "Erick";
+        Console.WriteLine(MyName);
+
+        int number = 21;
+        Console.WriteLine(number);
+
+        double numberDouble = 103.75;
+        Console.WriteLine(numberDouble);
+
+        char character = 'a';
+        Console.WriteLine(character);
+
+        bool boolean = true;
+        Console.WriteLine(boolean);
+    }
+}
