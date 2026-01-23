c23a7aa3040168a7d358f4987bf8d14a3490bdd3
erick yuji hirata <134017989+ErickHirata@users.noreply.github.com>
2026-01-22 23:11:46 -0300

    Adicao do Projeto Basico de Input e OutPut

---
+++ Input e Output/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Input_Output;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("---------CADASTRO DE USUÁRIOS---------");
+    
+    // Solicita o nome do usuário
+        Console.Write("Digite seu nome: ");
+        string name = Console.ReadLine();
+
+    // Solicita a idade do usuario
+        Console.Write("Digite Sua Idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-----------------------------------");
+
+        Console.WriteLine("Nome do Usuário Cadastrado com sucesso: " + name);
+        Console.WriteLine("Idade do Usuário Cadastrado com sucesso: " + age);
+    }
+}
