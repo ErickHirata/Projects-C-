0eb32519a508f7e733535206421ab934fe80e596
erick yuji hirata <134017989+ErickHirata@users.noreply.github.com>
2026-01-25 19:35:15 -0300

    Adicao do Projeto Basico de Estrutura Condicional: If-Else

---
+++ Estrutura Condicional If-Else/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Estrutura_Condicional_If_Else;
+
+public class Program
+{
+   public static void Main()
+   {
+        Console.WriteLine("=============Verificador de Números Pares ou Ímpares=============");
+        Console.WriteLine("Digite um Número: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int result = number % 2;
+        if (result == 0)
+        { 
+            Console.WriteLine("O Número digitado é PAR!");
+        }
+            else 
+            { 
+            Console.WriteLine("O Número digitado é IMPAR!");
+            }
+    }
+}
