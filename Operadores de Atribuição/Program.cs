13a69df0b4ee68bbbed9b0f3ab54fef7d6798b27
erick yuji hirata <134017989+ErickHirata@users.noreply.github.com>
2026-01-25 19:58:30 -0300

    Adicao do Projeto Basico de Operador de Atribuição

---
+++ Operadores de Atribuição/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Operadores_de_Atribuicao;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("============Operadores de Atribuição============");
+
+        //Inicializar a variavel
+        int x = 10;
+        Console.WriteLine("\nValor Inicial da Variável de x: " + x);
+
+        //Atribuição Simples (=)
+        x = 20;
+        Console.WriteLine("\nAtribuição Simples (=)");
+        Console.WriteLine("Valor de x: " + x);
+
+        //Atribuição Composta (Incremento) (++)
+        x++;  //Equivale a    x = x + 1
+            Console.WriteLine("\nAtribuição Composta Incremento (++)");
+            Console.WriteLine("Novo Valor de X:  " + x);
+
+        //Atribuição Composta (+=)
+        x += 10; //Equivale a   x = x + 10
+        Console.WriteLine("\nAtribuição Composta Incremento (+=)");
+        Console.WriteLine("Novo Valor de X:  " + x);
+
+        //Atribuição Composta (-=)
+        x -= 10; //Equivale a   x = x - 10
+        Console.WriteLine("\nAtribuição Composta Incremento (-=)");
+        Console.WriteLine("Novo Valor de X:  " + x);
+
+        //Atribuição Composta (*=)
+        x *= 10; //Equivale a   x = x * 10
+        Console.WriteLine("\nAtribuição Composta Incremento (*=)");
+        Console.WriteLine("Novo Valor de X:  " + x);
+
+        //Atribuição Composta (/=)
+        x /= 10; //Equivale a   x = x * 10
+        Console.WriteLine("\nAtribuição Composta Incremento (/=)");
+        Console.WriteLine("Novo Valor de X:  " + x);
+    }
+
+}
